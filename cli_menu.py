231cd3af93b0a995e6f2b7aff3162eb532fea128
jeanslack <jeanlucperni@gmail.com>
2015-02-24 22:15:08 +0100

    1 update commit

--- cli_menu.py
+++ cli_menu.py
@@ -32,12 +32,12 @@ class Appetite(object):
 		Is None if batch file process (-b) arg. 
 		"""
 		
-		self.input_format = input_format 
+		self.input_format = input_format
 		
 		self.retcode = None # return a data tuple
 		
 		
-	def input_selector(self):
+	def input_selector(self, input_selection):
 		"""
 		evaluate the input key choose; return the attribute input_format
 		and pass int number at the output_selection method
@@ -45,30 +45,19 @@ class Appetite(object):
 		case = {'1':'wav', '2':'aiff', '3':'flac', '4':'ape', '5':'mp3',
 				'6':'ogg',}
 		
-		subprocess.call(['clear'])
-		
-		main_menu()
-		
-		input_selection = raw_input("Enter here the corresponding number "
-									"and hit enter... "
-									)
-		
-		if input_selection in case.keys():
+		if input_selection in case.keys(): # keys sono numeri str
 			
-			self.input_format = case[input_selection]
-			self.output_selector(int(input_selection))
-			
-		elif input_selection == 'q' or input_selection == 'Q':
-			sys.exit()
-			
+			self.input_format = case[input_selection] # mi da il formato
+		
 		else:
-			sys.exit("\n\033[1mEntry error in select input format!\033[0m\n")
-			
+			self.input_format = None
+			return self.input_format
+		
 		return self.input_format
 		
 		
 		
-	def output_selector(self, remove_this):
+	def output_selector(self, output_selection):
 		"""
 		Show a graphic audio formats list without the input format 
 		for combine the user choice. This method can be called outside
@@ -84,35 +73,18 @@ class Appetite(object):
 				'f':'ogg', 'F':'ogg',
 				}
 		
-		graphic_a_format = output_menu()
-		self.new = graphic_a_format[:] # make a new list
-		self.new.remove(graphic_a_format[remove_this]) # remove input format 
-		
-		subprocess.call(['clear'])
-
-		print ('\n\n    The audio input files format is "%s" \n\n'
-				'    Please, now type the output files format for '
-				'encoding\n' % self.input_format)
-		
-		for outformat in self.new:
-			print "    %s"%(outformat)
-			
-		output_selection = raw_input(
-					'\n Type a letter for your encoding and just hit enter: ')
-		
 		if output_selection in case.keys():
 			
 			output_format = case[output_selection]
-			
 			self.diction_strings(self.input_format, output_format)
-			
-		elif output_selection == 'q' or output_selection == 'Q':
-			
-			sys.exit()
 			
 		else:
 			
-			sys.exit("\n\033[1mEntry error in select output format!\033[0m\n")
+			output_format = None
+			return output_format
+		
+		return output_format
+	
 			
 			
 	def diction_strings(self, input_format, output_format):
@@ -121,34 +93,29 @@ class Appetite(object):
 		compared with a database (dictionary) from which returns 
 		data for that pair
 		"""
-		
 		comparision = '%s > %s' % (input_format, output_format)
 
 		self.retcode = dictionaries(comparision)
 		
 		
 		
-	def quality_level(self, dict_bitrate, graphic_bitrate, dialog):
+	def quality_level(self, dict_bitrate, level):
 		"""
 		If audio codec support audio quality or bitrate, this method 
 		return a level of bitrate.
 		"""
 		
-		subprocess.call(['clear'])
-		
-		print graphic_bitrate
-		
-		level = raw_input(dialog)
-		
 		if level in dict_bitrate:
 			
-			return dict_bitrate[level]
-		
-		elif level == 'c' or level == 'C':
+			valid = True
 			
-			sys.exit()
+			return dict_bitrate[level]
 			
 		else:
+			
+			valid = False
+
+			return valid
 		
-			sys.exit("\n\033[1m Error\033[0m, inexistent quality level\n")
+		
 			
