727b0ab514bb6daebc5157e7a0b9b92fc154e305
Gianluca Pernigotto <jeanlucperni@gmail.com>
2017-12-07 01:11:20 +0100

    update

--- admspack/process_batch.py
+++ admspack/process_batch.py
@@ -20,27 +20,27 @@ from comparisions import a_formats, output_menu, build_cmd
 warnings = 'audiomass: \033[33;7;3mWarning!\033[0m'
 errors = 'audiomass: \033[31;7;3mError!\033[0m'
 
+# indx: range indica gli indici per i formati non supportati in 
+# lista graphic_out_formats data da output_menu() solo per i formati definiti 
+# in variabile f_limit, perche mp3, ogg, ape, is available decoding in 
+# wav/aiff only:
+f_limit = ['mp3','ogg','ape','MP3','OGG','APE']
+indx = 2,3,4,5,6
+
 def batch_parser(f_list, path_O):
     """
-    1- removal of any duplicates in the list
-    2- dictionary creation key = format: value = [filename, filename, etc] 
-      in order to group the streams with the same format into separate lists 
-    3- Creation input menu
-    4- Creation output menu
-    During output menu creation, Get a tuple with specified command
-    elements
+    Clean-up list contaminated by duplicate files.
     """
-    ##########################  RIMOZIONE DUPLICATI
-    # Clean-up list contaminated by duplicate files.
     for k,v in Counter(f_list).items():# controllo doppioni accidentali.
         if v>1:
             print "%s Removing following duplicates: > '%s' >" % (warnings, k)
-    f_list = list(set(f_list)) # elimino eventuali doppioni
+    f_list = list(set(f_list)) # removal of any duplicates in the list
+    sorting_dictionary(f_list, path_O)
+
+def sorting_dictionary(f_list, path_O):
     """
-    ##########################  CREAZIONE DIZ 
-    The following block of code is for sorting input formats into 
-    separate lists
-    ORDINAZIONE SEPARATA IN LISTE PER CIASCUN FILE FORMAT.
+    Dictionary creation key = format: value = [filename, filename, etc] 
+    in order to group the streams with the same format into separate lists.
     """
     formats = {}#Dizionario {formato1:[file1,file2,etc],..]}
     new_list = []#La lista ripulita dai file non supportati
@@ -51,8 +51,6 @@ def batch_parser(f_list, path_O):
       new = ext.replace(".","")#Tolgo il punto all'estensione
       if new in supported_formats[1]:#Se estensione input è supportata
         new_list.append(i)#Lista ripulita file importati
-
-
         if not formats.has_key(new.lower()):#Add not present key at dict
           formats[new.lower()] = [] #Aggiungo chiavi non presenti nel diz +
                             #valore lista vuota: {formato:[]}
@@ -66,69 +64,61 @@ def batch_parser(f_list, path_O):
       name, ext = os.path.splitext(i)
       new = ext.replace(".","")# tolgo il punto all'estensione
       formats[new.lower()].append("%s%s" %(name,ext))
-      
-    ##########################  CREAZIONE INPUT MENU 
+    menu_selections(formats, supported_formats, path_O)
+
+def menu_selections(formats, supported_formats, path_O):
+    """
+    Creation input menu
+    Creation output menu
+    During output menu creation, Get a tuple with specified command
+    elements
+    """
     input_selection = []#Contiene solo interi(int)
     graphic_out_formats = output_menu()#Lista menu per i formati in uscita
-    """
-    indx: range che indica gli indici per i formati non supportati in 
-    lista graphic_out_formats data da output_menu() solo per i formati definiti 
-    in variabile f_limit, perche mp3, ogg, ape, is available decoding in 
-    wav/aiff only:
-    """
-    f_limit = ['mp3','ogg','ape','MP3','OGG','APE']
-    indx = 2,3,4,5,6
+
     for input_format in formats.keys():#Itero sui formati importati
-      """
-      NOTE 1 RELOAD: ricarico nuovamente il grafico dei formati 
-                    integralmente con l'originale 
-      """
+      # NOTE 1 RELOAD: ricarico nuovamente il grafico dei formati 
+      # integralmente con l'originale 
       new = graphic_out_formats[:] # RELOAD
       print ("\n    Available formats for encoding/decoding "
           "'\033[32;1m%s\033[0m' audio stream" % input_format)
-      """
-      Dizion. = {chiavi'srtinga 1':(integear,'formato')} 
-      itero sulla tupla valori 
-      """
+      # Dizion. = {chiavi'srtinga 1':(integear,'formato')} 
+      # itero sulla tupla valori 
       for v in supported_formats[0].values():
         if input_format in v:# mi prendo gli interi corrispondenti
-          """
-          input_selection contiene solo integear, che sono il primo elemento
-          del valore tupla della chiave del dizionario 'supported_formats' 
-          corrispondente al formato (vedi supported_formats in 
-          comparisions.a_formats()
-          """
+          # input_selection contiene solo integear, che sono il primo elemento
+          # del valore tupla della chiave del dizionario 'supported_formats' 
+          # corrispondente al formato (vedi supported_formats in 
+          # comparisions.a_formats()
           input_selection.append(v[0])# v[0] mi da l'intero
 
           if input_format in f_limit:
               new = [ new[i] for i in xrange(len(new)) if i not in set(indx) ]
           else:
-              """
-              NOTE 2 SET: qui con l'intero ottenuto rimuovo dalla lista
-                          i formati che non sono trattati o incompatibili.
-              """
+             # NOTE 2 SET: qui con l'intero ottenuto rimuovo dalla lista
+             # i formati che non sono trattati o incompatibili.
               new.remove(graphic_out_formats[v[0]])# SET
           for outformat in new:# realizzazione menu di output
             print "    %s"%(outformat)
           output_selection = raw_input('    Choice a format by a letter '
                                         'and just hit enter: ')
-          """NOTE 3 DELETE: cancello il grafico dei formati settati prima
-            altrimenti rimangono in memoria con gli elementi gia rimossi"""
+          # NOTE 3 DELETE: cancello il grafico dei formati settati prima
+          # altrimenti rimangono in memoria con gli elementi gia rimossi
           del new[:] # DELETE
-          
-          ######################  CREAZIONE OUTPUT MENU 
+          #------------------- CREAZIONE OUTPUT MENU ------------------#
           main = Audio_Formats(input_format)# Have a ext input >
           output_format = main.output_selector(output_selection)# get out format
           tuple_data = main.diction_strings()# return a tuple data of the codec
           if output_selection == 'q' or output_selection == 'Q':
             sys.exit()
           elif output_format is None:
+            # Se nessuna selezione e premi enter rimuovo  chiave e valore 
+            # dal dizionario, cioè escludo quei files dalla conversione.
             print ("\n%s Entry error in selection output format, "
                      " %s >> skipping" % (warnings, output_selection))
-            formats.pop(input_format, None)#Se nessuna selezione e premi enter
-            #rimuovo  chiave e valore dal dizionario, cioè escludo quei files
-            #dalla conversione.
+            formats.pop(input_format, None)
             continue # meglio partire da capo 
+
           if tuple_data == 'KeyError':
             print ("\n%s Incompatible conversion >> skipping >>" % warnings)
             formats.pop(input_format, None)
@@ -191,8 +181,8 @@ def command_builder(tuple_data, bitrate, output_format, path_in, path_O):
         print ("\n\033[36;7m|%s| %s Output Stream:\033[0m >> '%s/%s.%s'\n" 
             % (str(count),output_format, path_O, file_name, output_format))
         try:
-            #print command # uncomment for debug
-            subprocess.check_call(command, shell=True)
+            print command # uncomment for debug
+            #subprocess.check_call(command, shell=True)
         except subprocess.CalledProcessError as err:
             sys.exit("audioamass:\033[31;1mERROR!\033[0m %s" % (err))
             
